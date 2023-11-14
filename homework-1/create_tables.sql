27cfcade1bc24a85aa299ff752d9223f0cc40699
hastya <hastya88.88@gmail.com>
2023-11-14 14:52:11 +0300

    Update create_tables.sql

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,23 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE customers (
+  customer_id SERIAL PRIMARY KEY,
+  company_name VARCHAR(100) NOT NULL,
+  contact_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE employees (
+  employee_id SERIAL PRIMARY KEY,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  title VARCHAR(100),
+  birth_date DATE,
+  notes TEXT
+);
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+	customer_id INT REFERENCES customers(customer_id),
+	employee_id INT REFERENCES employees(employee_id),
+    order_date DATE NOT NULL,
+    ship_city VARCHAR(50) NOT NULL
+);
